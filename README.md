3240f2a9f53c14483e54c8bb7ad3fb465b1ca949
Justin Kumpe <jakumpe@kumpes.com>
2025-01-25 14:58:57 -0600

    init

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ git clone https://github.com/justinkumpe/rpi-servo-controller.git
 ## Instructions
 ```
 cd rpi-servo-controller
-
+chmod +x set-servo-angle.py
+./set-servo-angle.py --servo-pin=8 --angle=180
 ```
--- set-servo-angle.py
+++ set-servo-angle.py
@@ -13,7 +13,6 @@ parser.add_argument("-a", "--angle", help="Angle to move to")
 parser.add_argument("-f", "--from_angle", help="Angle to move from", required= False)
 
 args=parser.parse_args()
-print(args)
 servo_pin: int = int(args.servo_pin)
 to_angle: int = int(args.angle)
 from_angle: int = int(args.from_angle) if args.from_angle else to_angle
@@ -42,7 +41,6 @@ servo.start(0)
 # Loop to allow user to set servo angle. Try/finally allows exit
 # with execution of servo.stop and GPIO cleanup :)
 def set_angle(angle: int):
-    print(f"set_angle to {angle}")
     if angle == 0:
         angle = -1
     servo.ChangeDutyCycle(2+(angle/18))
@@ -55,13 +53,8 @@ try:
         for angle in range(from_angle, to_angle, step):
             set_angle(angle)
             time.sleep(0.1)
-        time.sleep(0.5)
-        servo.ChangeDutyCycle(0)
     else:
         set_angle(to_angle)
-        time.sleep(0.5)
-        servo.ChangeDutyCycle(0)
-
 finally:
-    #Clean things up at the end
-    print("Goodbye!")
+    time.sleep(0.5)
+    servo.ChangeDutyCycle(0)
