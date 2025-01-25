876af228a2a130f0704064ab7cbbdd987df40539
Justin Kumpe <jakumpe@kumpes.com>
2025-01-25 15:12:11 -0600

    init

--- set-servo-angle.py
+++ set-servo-angle.py
@@ -8,14 +8,15 @@ import argparse
 
 parser=argparse.ArgumentParser()
 
-parser.add_argument("-p", "--servo-pin", help="Board Pin number for SERVO motor")
-parser.add_argument("-a", "--angle", help="Angle to move to")
+parser.add_argument("-p", "--servo-pin", help="Board Pin number for SERVO motor", required= False, default= "8")
+parser.add_argument("-a", "--angle", help="Angle to move to", required= False, default= "0")
 parser.add_argument("-f", "--from_angle", help="Angle to move from", required= False)
 
 args=parser.parse_args()
 servo_pin: int = int(args.servo_pin)
 to_angle: int = int(args.angle)
 from_angle: int = int(args.from_angle) if args.from_angle else to_angle
+
 # Set GPIO numbering mode
 GPIO.setmode(GPIO.BOARD)
 
